4fe741566f8c00ff36e8861e2fa4b7f55effc428
illuminate3 <carsten.sapporo@gmail.com>
2015-04-12 22:47:04 -0500

    add cms

---
+++ src/Illuminate3/lang/en/cms.php
@@ -0,0 +1,64 @@
+<?php
+
+// shop
+
+
+return [
+
+	'asset'							=> 'Asset|Assets',
+	'asset_status'					=> 'Asset Status|Asset Statuses',
+	'asset_tag'						=> 'Asset Tag',
+	'barcode'						=> 'Barcode',
+	'item'							=> 'Item|Items',
+	'make'							=> 'Make',
+	'model'							=> 'Model',
+	'model_number'					=> 'Model Number',
+	'po'							=> 'PO #',
+	'scan'							=> 'Scan|Scans',
+	'serial'						=> 'Serial #',
+	'tech_status'					=> 'Tech Status|Tech Statuses',
+
+// ask
+	'ask' => [
+// 		'delete'					=> 'Delete this Module?',
+	],
+
+// button
+	'button' => [
+// 		'add'						=> 'Add Module',
+// 		'create'					=> 'Create New Module',
+// 		'delete'					=> 'Delete Module',
+// 		'edit'						=> 'Edit Module',
+// 		'new'						=> 'New Module',
+// 		'view'						=> 'View Module',
+// 		'update'					=> 'Update Module',
+	],
+
+// command
+	'command' => [
+// 		'create'					=> 'Create New Module',
+// 		'edit'						=> 'Edit Module',
+// 		'delete'					=> 'Delete Module',
+// 		'update'					=> 'Update Module',
+	],
+
+// error
+	'error' => [
+// 		'create'					=> 'There was an issue creating the Module. Please try again.',
+// 		'delete'					=> 'There was an issue deleting the Module. Please try again.',
+// 		'exists'					=> 'Module already exists!',
+// 		'installed'					=> 'Already installed!',
+// 		'name_required'				=> 'The Name Field is Required.',
+// 		'not_exist'					=> 'Module [:id] does not exist.',
+// 		'not_found'					=> 'No Modules found.',
+// 		'update'					=> 'There was an issue updating the Module. Please try again.',
+	],
+
+// success
+	'success' => [
+		'page_create'				=> 'The Page was successfully created.',
+		'page_update'				=> 'The Page was successfully updated.',
+	],
+
+
+];
