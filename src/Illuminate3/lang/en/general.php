744fe67c5da5b8827735c73f6e5e79523e731bd1
Carsten <carsten@cogents.io>
2015-04-29 14:58:31 -0500

    cc

--- src/Illuminate3/lang/en/general.php
+++ src/Illuminate3/lang/en/general.php
@@ -72,6 +72,21 @@ return [
 	'website'						=> 'Web Site',
 	'yes'							=> 'Yes',
 
+// months
+	'month_1'						=> 'January',
+	'month_2'						=> 'February',
+	'month_3'						=> 'March',
+	'month_4'						=> 'April',
+	'month_5'						=> 'May',
+	'month_6'						=> 'June',
+	'month_7'						=> 'July',
+	'month_8'						=> 'August',
+	'month_9'						=> 'September',
+	'month_10'						=> 'October',
+	'month_11'						=> 'November',
+	'month_12'						=> 'December',
+
+
 // ask
 	'ask' => [
 		'create'					=> 'Continue?',
