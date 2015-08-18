eff7b5742e1a1f804814810240deac3047ef17b5
Carsten <carsten@cogents.io>
2015-08-18 10:40:28 -0500

    new file : files

---
+++ src/Illuminate3/lang/en/files.php
@@ -0,0 +1,23 @@
+<?php
+
+// files
+
+
+return [
+
+	'contact_information'			=> 'Contact Information',
+	'document'						=> 'Document|Documents',
+
+// ask
+// 	'ask' => [
+// 		'isSupervisior'				=> 'Supervisior ?',
+// 	],
+
+// success
+	'success' => [
+		'document_create'			=> 'The Document was successfully created.',
+		'document_delete'			=> 'The Document was successfully deleted.',
+	],
+
+
+];
