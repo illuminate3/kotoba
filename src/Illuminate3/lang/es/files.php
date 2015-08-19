84a7e138dc20d04d23f612b3e985eb42e8f4737c
Carsten <carsten@cogents.io>
2015-08-19 12:28:24 -0500

    files: image success

--- src/Illuminate3/lang/es/files.php
+++ src/Illuminate3/lang/es/files.php
@@ -17,6 +17,8 @@ return [
 	'success' => [
 		'document_create'			=> 'The Document was successfully created.',
 		'document_delete'			=> 'The Document was successfully deleted.',
+		'image_create'				=> 'The Image was successfully created.',
+		'image_delete'				=> 'The Image was successfully deleted.',
 	],
 
 
